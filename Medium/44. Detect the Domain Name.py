0be09a84f89a6d8fe2e304af59a7b13b140e8393
Saswat Seth <109916456+Cyberoctane29@users.noreply.github.com>
2025-11-23 11:47:07 +0530

    more

--- Medium/44. Detect the Domain Name.py
+++ Medium/44. Detect the Domain Name.py
@@ -1,6 +1,6 @@
 # Problem: Detect the Domain Name  
 # Difficulty: Medium  
-# Skills: Python, Regular Expressions (re module), String Etraction  
+# Skills: Python, Regular Expressions (re module), String Extraction  
 
 # Problem Statement:
 # You are given N lines of an HTML fragment. Your task is to extract **unique domain names**
